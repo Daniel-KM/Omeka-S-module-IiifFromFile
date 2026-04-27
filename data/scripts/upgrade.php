5552e1c394c6792c2eee2ea3ceef279fe281ce2d
Daniel Berthereau <Daniel.git@Berthereau.net>
2026-04-27 00:00:00 +0000

    Released version 3.4.1.

---
+++ data/scripts/upgrade.php
@@ -0,0 +1,44 @@
+<?php declare(strict_types=1);
+
+namespace IiifFromFile;
+
+use Common\Stdlib\PsrMessage;
+
+/**
+ * @var Module $this
+ * @var \Laminas\ServiceManager\ServiceLocatorInterface $services
+ * @var string $newVersion
+ * @var string $oldVersion
+ *
+ * @var \Omeka\Api\Manager $api
+ * @var \Laminas\Log\Logger $logger
+ * @var \Omeka\Settings\Settings $settings
+ * @var \Omeka\Settings\SiteSettings $siteSettings
+ * @var \Doctrine\DBAL\Connection $connection
+ * @var \Doctrine\ORM\EntityManager $entityManager
+ * @var \Omeka\Mvc\Controller\Plugin\Messenger $messenger
+ */
+$plugins = $services->get('ControllerPluginManager');
+$api = $plugins->get('api');
+$logger = $services->get('Omeka\Logger');
+$settings = $services->get('Omeka\Settings');
+$translate = $plugins->get('translate');
+$connection = $services->get('Omeka\Connection');
+$messenger = $plugins->get('messenger');
+$siteSettings = $services->get('Omeka\Settings\Site');
+$entityManager = $services->get('Omeka\EntityManager');
+
+if (!method_exists($this, 'checkModuleActiveVersion') || !$this->checkModuleActiveVersion('Common', '3.4.85')) {
+    $message = new \Omeka\Stdlib\Message(
+        $translate('The module %1$s should be upgraded to version %2$s or later.'), // @translate
+        'Common', '3.4.85'
+    );
+    $messenger->addError($message);
+    throw new \Omeka\Module\Exception\ModuleCannotInstallException((string) $translate('Missing requirement. Unable to upgrade.')); // @translate
+}
+
+if (version_compare((string) $oldVersion, '3.4.1', '<')) {
+    $messenger->addSuccess(new PsrMessage(
+        'IIIF from File now supports Dataverse repositories alongside Nakala, exposes ingester and post-deposit media-handling options, and the sync action gains three modes (complete, overwrite, replace) along with per-value language tags.' // @translate
+    ));
+}
